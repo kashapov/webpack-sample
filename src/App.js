c89ce8254fcc8d95ee230a92a4053b97db71bd66
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-21 20:13:31 +0300

    Adding CSS File Support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
 
 import Users from "./containers/Users";
-import asyncComponent from './hoc/asyncComponent';
+import asyncComponent from "./hoc/asyncComponent";
 
 const AsyncPizza = asyncComponent(() => {
-  return import('./containers/Pizza');
+  return import("./containers/Pizza");
 });
 
 class App extends Component {
@@ -13,8 +13,7 @@ class App extends Component {
     return (
       <div>
         <div>
-          <Link to="/">Users</Link> |
-          <Link to="/pizza">Pizza</Link>
+          <Link to="/">Users</Link> |<Link to="/pizza">Pizza</Link>
         </div>
         <div>
           <Route path="/" exact component={Users} />
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,48 @@
-const path = require('path');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: './src/index.js',
+  devtool: "cheap-module-eval-source-map",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: ""
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
+        exlude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]__[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [
+                autoprefixer({
+                  browsers: ["> 1%", "last 2 versions"]
+                })
+              ]
+            }
+          }
+        ],
         exlude: /node_modules/
       }
     ]
