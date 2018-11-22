c309988f436da711bc72b0553372ec5e6d5074cc
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-22 22:36:07 +0300

    Injecting Script to index.html

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div>
         <div>
-          <Link to="/">Users</Link> |<Link to="/pizza">Pizza</Link>
+          <Link to="/">Users</Link> | <Link to="/pizza">Pizza</Link>
         </div>
         <div>
           <Route path="/" exact component={Users} />
@@ -23,3 +23,5 @@ class App extends Component {
     );
   }
 }
+
+export default App;
